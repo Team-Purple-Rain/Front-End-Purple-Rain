37bb270ff574e62fa0e50efdd58c09910026244f
Chloe Mintel <mintelc12@gmail.com>
2022-08-14 18:52:24 -0400

    we got the service worker registered.

--- that-guide/src/App.js
+++ that-guide/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [baseURL, setBaseURL] = useState("https://thatguide.herokuapp.com");
+  const [description, setDescription] = useState("");
+  const [memeImage, setMemeImage] = useState("");
+  const [team, setTeam] = useState("");
+
+  useEffect(() => {
+    axios.get(`${baseURL}`).then((res) => {
+      setDescription(res.data.description);
+      setMemeImage(res.data.meme_image);
+      setTeam(res.data.team);
+    });
+  });
   return (
     <>
-    <h1>T.H.A.T. Guide</h1>
-    <h3>Your guide to the Appalachian Trail.</h3>
+      <h2>
+        {" "}
+        Hey {team}! {description}{" "}
+      </h2>
+      <img src={memeImage} width="250px" />
     </>
   );
 }

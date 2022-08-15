8d3c67fd9504bd3a3b934474f6981b5619887d43
Chloe Mintel <mintelc12@gmail.com>
2022-08-15 09:22:28 -0400

    homepage component has been made and we are going work on different parts of the component as assigned in Trello.

--- that-guide/src/App.js
+++ that-guide/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Homepage from "./Homepage"
 
 function App() {
   const [baseURL, setBaseURL] = useState("https://thatguide.herokuapp.com");
@@ -9,22 +10,44 @@ function App() {
   const [memeImage, setMemeImage] = useState("");
   const [team, setTeam] = useState("");
 
-  useEffect(() => {
-    axios.get(`${baseURL}`).then((res) => {
-      setDescription(res.data.description);
-      setMemeImage(res.data.meme_image);
-      setTeam(res.data.team);
-    });
-  });
-  return (
+
+
+return (
     <>
-      <h2>
-        {" "}
-        Hey {team}! {description}{" "}
-      </h2>
-      <img src={memeImage} width="250px" />
+    <div className="title-header">
+        <h1>T.H.A.T. Guide</h1>
+        <h3>Your interactive guide to the Appalachian Trail.</h3>
+    </div>
+    <div className="nav-bar">
+      <h3>(Here is where our navbar might go, scooter or beyond version) </h3>
+    </div>
+
+    <Homepage />
+    
     </>
-  );
+);
+
+
+
+
+
+// Lisa's test of our endpoints below:
+//   useEffect(() => {
+//     axios.get(`${baseURL}`).then((res) => {
+//       setDescription(res.data.description);
+//       setMemeImage(res.data.meme_image);
+//       setTeam(res.data.team);
+//     });
+//   });
+//   return (
+//     <>
+//       <h2>
+//         {" "}
+//         Hey {team}! {description}{" "}
+//       </h2>
+//       <img src={memeImage} width="250px" />
+//     </>
+//   );
 }
 
 export default App;
